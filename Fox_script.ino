c416a121d811ad2d44bb4d6d41d31a4f0025d339
tj-murphy <tjmurphy@mit.edu>
2019-03-15 20:26:17 -0400

    Removed receive function; changed pinouts to match Bao-foxes

--- Fox_script.ino
+++ Fox_script.ino
@@ -4,21 +4,18 @@ String alphabet = alphabet1+alphabet2;
 
 //Parameter which indirectly sets morsing speed. Higher = slower. Defined in milliseconds.
 //Rule of thumb I derived: 1250 / this number = WPM.
-int dotDuration = 150;
+int dotDuration = 75;
 //This message is what will be transmitted in Morse.
 String programMessage = "W1XM FOX HUNT AT MIT";
-//Amount of time to wait between transmissions if the hunters have not been heard. In seconds.
+//Amount of time to wait between transmissions. In seconds.
 unsigned long transmitDelay = 150;
 
 //Pull low when you want to send a message. Equivalent to PTT button on a radio.
 int TXSelectPin = 7;
 //Carries our CW message
-int TXSignalPin = 6;
+int TXSignalPin = 3;
 
-int RXSelectPin = 9;
-int RXDetectPin = A2;
-
-int LEDPin = 8;
+int LEDPin = 13;
 
 //Tracks the last time we transmitted, so we can do that automatically.
 unsigned long lastTransmissionTime = 0;
@@ -46,11 +43,11 @@ String getMorse(char letter)
 
 void transmit(String message)
 {
-  digitalWrite(RXSelectPin,HIGH);
-  digitalWrite(TXSelectPin,LOW);
+  digitalWrite(TXSelectPin,HIGH);
   //Give a moment for the radio board to turn on
   delay(200);
-  message.toUpperCase();
+  message.toUpperCase
+  ();
   //Transmit each letter of the message
   for(int i=0; i<message.length(); i++)
   {
@@ -71,10 +68,9 @@ void transmit(String message)
     }
    delay(dotDuration*3);
   }
-  digitalWrite(TXSelectPin,HIGH);
+  digitalWrite(TXSelectPin,LOW);
   //Safety element to make sure everything can shut down after transmitting. Don't want to receive ourselves or anything like that.
   delay(1000);
-  digitalWrite(RXSelectPin,LOW);
   lastTransmissionTime = millis();
 }
 void buzz(int duration)
@@ -90,39 +86,14 @@ void setup() {
   digitalWrite(TXSelectPin,HIGH);
   pinMode(TXSignalPin,OUTPUT);
   digitalWrite(TXSignalPin,LOW);
-  pinMode(RXSelectPin,OUTPUT);
   //Want standard state to be receiving.
-  digitalWrite(RXSelectPin,LOW);
-  pinMode(RXDetectPin,INPUT);
   pinMode(LEDPin,OUTPUT);
   digitalWrite(LEDPin,LOW);
   //Once we're up and running, first order of business is to do a transmission.
   transmit(programMessage);
 }
 
-void loop() {
-    //When idle, random environmental noise causes us to get a random wave which goes between about 200 and 400 analogRead units.
-    //When a transmission is received, the value goes very solidly to ~312. If the value goes far outside this, we are not receiving.
-    //Listen for 1 second to see if this is the case.
-    unsigned long startTime = millis();
-    int maxLevel = 0;
-    int minLevel = 1024;
-    int RXLevel = 320;
-    while(millis() < startTime+1000)
-    {
-      RXLevel = analogRead(RXDetectPin);
-      maxLevel = max(maxLevel,RXLevel);
-      minLevel = min(minLevel,RXLevel);
-    }
-    //Meaning, the level always stayed within 300 to 350, indicating a received signal
-    if(maxLevel<350 and minLevel > 300)
-    {
-      //Wait 3 seconds so the hunters can stop talking and be ready to listen.
-      delay(3000);
-      //Short message here for now. Don't want to clog the air.
-      transmit("W1XM");
-    }
-    
+void loop() {    
     if (millis() > (lastTransmissionTime + 1000*transmitDelay))
     {
       transmit(programMessage);
